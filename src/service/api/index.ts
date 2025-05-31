45b7c0c89dc02ffa41eaaca78b00579831acb653
AlvaroDeCarvalho <alvarodecarvalho20@gmail.com>
2025-05-31 01:19:22 -0300

    erro de redirecionamento

--- src/service/api/index.ts
+++ src/service/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const url = "https://api.aprendizado.quantisoft.com.br/api/v1" 
-const url = "http://localhost:8080/api/v1"
+const url = "https://api.aprendizado.quantisoft.com.br/api/v1" 
+//const url = "http://localhost:8080/api/v1"
 export const API = axios.create({
     baseURL: url,
     headers: {
